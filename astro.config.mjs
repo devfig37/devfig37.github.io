b986d023a51c4c740e14cbecc0f2d74f49cb2816
Hector Figueroa <hfigueroa37@gmail.com>
2024-12-31 17:02:44 -0800

    normalizing path in navbar for selection color

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-const isProd = import.meta.env.PROD;
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://devfig37.github.io',
-    // base: isProd === true ? '/devfig37' : '/',
 });
